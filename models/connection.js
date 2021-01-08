9a864caa5a0e2ebc014f9ce2654ba41da1676559
henrique <hbaetaleite@gmail.com>
2021-01-07 23:34:48 -0300

    limpando connection, passando URL para o dotenv

--- models/connection.js
+++ models/connection.js
@@ -2,15 +2,13 @@ const mongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 require('dotenv/config');
 
-const password = process.env.PASSWORD;
-const dbname = 'baetaLeiteLivros';
 const DB_NAME = 'baetaLeiteLivros';
 
-const URI = `mongodb+srv://baetaLeite:${password}@libraryleite.cw0bi.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const URL = process.env.URL;
 
 const connection = () =>
   mongoClient
-    .connect(URI, {
+    .connect(URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
@@ -19,7 +17,6 @@ const connection = () =>
     })
     .catch((err) => {
       console.error(err);
-      // process.exit(1);
       throw err;
     });
 
